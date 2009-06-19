4a6ea002ba2db65d3d2736f0bc227c77494872f8
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-06-19 16:52:17 +0000

    Moving mutant plugin to the new structure

---
+++ toxtree-plugins/toxtree-mutant/src/main/java/mutant/descriptors/DescriptorBridgedBiphenyl.java
@@ -0,0 +1,43 @@
+package mutant.descriptors;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+
+import toxTree.tree.rules.smarts.ISmartsPattern;
+import toxTree.tree.rules.smarts.SmartsPatternCDK;
+
+public class DescriptorBridgedBiphenyl extends DescriptorStructurePresence<IAtomContainer> {
+	protected static String BiBr = "I(BiBr)";
+
+	public DescriptorBridgedBiphenyl() {
+		super();
+		try {
+		setParameters(new Object[] {
+//				"c1ccccc1!@[*;!N]!@c2ccccc2",
+				//"[$(c1ccccc1!@[*;!N]!@c2ccccc2),$(c1ccccc1!@[N]!@[$(ccN),$(cccN),$(ccccN)])]",
+				"[$(c1ccccc1!@[*;!N]!@c2ccccc2),$(c1ccccc1!@[N]!@c2c(N)cccc2),$(c1ccccc1!@[N]!@c2cc(N)ccc2),$(c1ccccc1!@[N]!@c2ccc(N)cc2),$(c1ccccc1!@[N]!@c2cccc(N)c2),$(c1ccccc1!@[N]!@c2ccccc(N)2)]",
+
+				//"c1ccccc1!@[N]!@c2ccccc2",
+				BiBr}
+				);
+		} catch (CDKException x) {
+
+			setResultName(BiBr);
+			logger.error(x);
+		}
+	}
+	
+	 public DescriptorSpecification getSpecification() {
+	        return new DescriptorSpecification(
+	            "True if contains bridged biphenyl group, false otherwise. The bridge should not be composed by a functional amino group.",
+	            this.getClass().getName(),
+	            "$Id: DescriptorBridgedBiphenyl.java  2007-08-21 10:31 nina$",
+	            "ToxTree Mutant plugin");
+	    }
+
+    @Override
+    protected ISmartsPattern<IAtomContainer> createSmartsPattern() {
+        return new SmartsPatternCDK();
+    }
+}
