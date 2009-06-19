0a29a2066c9b6c9e33662791ac06974af4970c7f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-06-19 16:34:08 +0000

    Moving sicret plugin to the new structure

---
+++ toxtree-plugins/toxtree-sicret/src/main/java/sicret/categories/CategoryCorrosive.java
@@ -0,0 +1,47 @@
+/*
+Copyright Ideaconsult Ltd.(C) 2006  
+Contact: nina@acad.bg
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+package sicret.categories;
+
+import toxTree.tree.DefaultCategory;
+
+/**
+ * Assigned when compound is estimated to be corrosive to skin. 
+ * @author Nina Jeliazkova nina@acad.bg
+ * <b>Modified</b> Dec 17, 2006
+ */
+public class CategoryCorrosive extends DefaultCategory {
+
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 0;
+
+	/**
+	 * 
+	 */
+	public CategoryCorrosive() {
+		super("Corrosive to skin",5);
+		setExplanation("Corrosive to skin (R34 or R35)");
+		
+		
+
+	}
+}
+
