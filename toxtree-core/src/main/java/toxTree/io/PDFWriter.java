67638d2560ca014dfbce92dd9ff0d1e135a3a8a7
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-06-19 15:22:01 +0000

    Refactoring to use CompoundsImageTools from ambit2-core jar

--- toxtree-core/src/main/java/toxTree/io/PDFWriter.java
+++ toxtree-core/src/main/java/toxTree/io/PDFWriter.java
@@ -44,7 +44,7 @@ import org.openscience.cdk.interfaces.IMoleculeSet;
 import org.openscience.cdk.io.formats.IResourceFormat;
 import org.openscience.cdk.tools.DataFeatures;
 
-import toxTree.ui.molecule.CompoundImageTools;
+import ambit2.core.io.CompoundImageTools;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.Image;
