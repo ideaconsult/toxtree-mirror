eda8499c810418558c05e9d336ca99bf303f9875
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-06-25 08:01:58 +0000

    moving cramer module from the core to a separate module

--- toxtree/toxtree-plugins/toxtree-cramer/src/test/java/toxTree/cramer/RuleHeterocyclicComplexSubstituentsTest.java
+++ toxtree/toxtree-plugins/toxtree-cramer/src/test/java/toxTree/cramer/RuleHeterocyclicComplexSubstituentsTest.java
@@ -22,7 +22,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
-package toxTree.test.tree.cramer;
+package toxTree.cramer;
 
 import toxTree.core.IDecisionRule;
 import toxTree.tree.cramer.RuleHeterocyclicComplexSubstituents;
