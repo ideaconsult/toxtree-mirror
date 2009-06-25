6954c1ce465ea11aca664e6f9d43eb542b7d53ca
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-06-25 18:42:15 +0000

    a wrapper for toxtree plugins as IMolecularDescriptor

---
+++ toxtree-core/src/main/java/toxTree/core/ToxTreeDescriptor.java
@@ -0,0 +1,90 @@
+package toxTree.core;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+
+import toxTree.exceptions.DecisionResultException;
+
+import ambit2.core.data.StringDescriptorResultType;
+
+public class ToxTreeDescriptor implements IMolecularDescriptor {
+	protected String[] paramNames = {"decisionTree"};
+	protected Object[] parameters = new Object[1];
+	protected transient IDecisionMethod decisionTree = null;
+	protected String[] descriptorNames =  new String[] {"default"};
+	
+	public DescriptorValue calculate(IAtomContainer mol) throws CDKException {
+		if (decisionTree == null) 
+			try {
+				decisionTree = (IDecisionMethod) Introspection.loadCreateObject(getParameters()[0].toString());
+			} catch (Exception x) {
+				throw new CDKException(x.getMessage());
+			}
+			
+		if (decisionTree == null) throw new CDKException("No decision tree!");
+		IDecisionResult result = decisionTree.createDecisionResult();
+		try {
+			result.classify(mol);
+			result.assignResult(mol);
+			descriptorNames[0] = result.getResultPropertyName();
+			
+			Object value = mol.getProperty(descriptorNames[0]);
+
+			return new DescriptorValue(
+						getSpecification(),
+						getParameterNames(),
+						getParameters(),
+						new StringDescriptorResultType(value==null?null:value.toString()),
+						descriptorNames
+						);				
+
+		} catch (DecisionResultException x) {
+			throw new CDKException(x.getMessage());
+		}
+	}
+
+	public IDescriptorResult getDescriptorResultType() {
+		return new StringDescriptorResultType("");
+	}
+
+	public String[] getParameterNames() {
+		return paramNames;
+	}
+
+	public Object getParameterType(String arg0) {
+		for (int i=0; i < paramNames.length;i++)
+			if (arg0.equals(paramNames[i]))
+			return parameters[i];
+		return null;
+	}
+
+	public Object[] getParameters() {
+		return parameters;
+	}
+
+	public DescriptorSpecification getSpecification() {
+        return new DescriptorSpecification(
+                "http://toxTree.sourceforge.net",
+                this.getClass().getName(),
+                decisionTree==null?getParameters()[0].toString():decisionTree.getTitle(),
+                "Toxtree plugin");
+	}
+
+	public void setParameters(Object[] arg0) throws CDKException {
+		for (int i=0; i < parameters.length;i++) {
+			if (arg0[i] instanceof IDecisionMethod) 
+				if ((decisionTree==null) || !getParameters()[i].equals(arg0[i].getClass().getName())) {
+					parameters[i] = arg0[i].getClass().getName();
+					decisionTree = (IDecisionMethod)arg0[i];					
+				} else {}
+			else
+				parameters[i] = arg0[i];
+		}
+		
+	}
+
+}
