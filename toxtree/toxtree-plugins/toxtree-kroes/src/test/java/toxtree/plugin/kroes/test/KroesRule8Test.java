ba4652307774717d5b49f09e8825e436de4fd31d
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-08-27 17:04:30 +0000

    switching off the popup window for the test

--- toxtree/toxtree-plugins/toxtree-kroes/src/test/java/toxtree/plugin/kroes/test/KroesRule8Test.java
+++ toxtree/toxtree-plugins/toxtree-kroes/src/test/java/toxtree/plugin/kroes/test/KroesRule8Test.java
@@ -6,12 +6,15 @@ import org.junit.Test;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IMolecule;
 
-import ambit2.base.config.Preferences;
-
+import toxTree.core.IDecisionInteractive;
 import toxTree.core.IDecisionResult;
+import toxTree.core.IDecisionRule;
+import toxTree.core.IDecisionRuleList;
 import toxTree.query.FunctionalGroups;
+import toxTree.tree.DecisionNode;
 import toxtree.plugins.kroes.Kroes1Tree;
 import toxtree.plugins.kroes.rules.KroesRule8;
+import ambit2.base.config.Preferences;
 
 public class KroesRule8Test {
 	KroesRule8 rule = new KroesRule8();
@@ -29,6 +32,15 @@ public class KroesRule8Test {
 	@Test
 	public void testClassI() throws Exception {
 		Kroes1Tree tree = new Kroes1Tree();
+		tree.setInteractive(false);
+		IDecisionRuleList rules = tree.getRules();
+		for (int i=0; i < rules.size();i++) {
+			IDecisionRule rule = rules.get(i);
+			if (rule instanceof DecisionNode) 
+				rule = ((DecisionNode)rule).getRule();
+			if (rule instanceof IDecisionInteractive) 
+				((IDecisionInteractive)rule).setInteractive(false);
+		}
 		IDecisionResult r = tree.createDecisionResult();
 		Preferences.setProperty(Preferences.STOP_AT_UNKNOWNATOMTYPES,"true");
 		IAtomContainer mol = FunctionalGroups.createAtomContainer("CCCCCCCC");
