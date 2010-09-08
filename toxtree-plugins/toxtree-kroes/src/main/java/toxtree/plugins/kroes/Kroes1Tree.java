96e6a8ebe51c008c03098ea710c35585f6977bb3
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-09-08 10:10:35 +0000

    Fix for "unmappable character for encoding UTF8" errors

--- toxtree-plugins/toxtree-kroes/src/main/java/toxtree/plugins/kroes/Kroes1Tree.java
+++ toxtree-plugins/toxtree-kroes/src/main/java/toxtree/plugins/kroes/Kroes1Tree.java
@@ -21,7 +21,7 @@ public class Kroes1Tree extends UserDefinedTree
         setTitle("Kroes TTC decision tree");
         setChanged();
         notifyObservers();
-        setExplanation("Kroes, R., Renwick, A.G., Cheeseman, M., Kleiner, J., Mangelsdorf, I., Piersma, A., Schilter, B., Schlatter, J., van Schothorst, F., Vos, J.G., Wurtzen, G. (2004). Structure based thresholds of toxicological concern (TTC): guidance for application to substances present at low levels in the diet. Food Chem. Toxicol. 42, 65–83");
+        setExplanation("Kroes, R., Renwick, A.G., Cheeseman, M., Kleiner, J., Mangelsdorf, I., Piersma, A., Schilter, B., Schlatter, J., van Schothorst, F., Vos, J.G., Wurtzen, G. (2004). Structure based thresholds of toxicological concern (TTC): guidance for application to substances present at low levels in the diet. Food Chem. Toxicol. 42 pp.65 83");
     }    
     
     private static final long serialVersionUID = 0x4b97b63a593def3bL;
