f2129c8c4fd06f629658ae6073426695d32cf946
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-10-19 05:41:10 +0000

    Fixed: replaced valency calculation based on CDK atom typing, instead of counting bond orders https://sourceforge.net/tracker/?func=detail&aid=3089703&group_id=152702&atid=785126

--- toxtree/toxtree-plugins/toxtree-cramer/src/test/java/toxTree/cramer/RuleHasOtherThanC_H_O_N_S2Test.java
+++ toxtree/toxtree-plugins/toxtree-cramer/src/test/java/toxTree/cramer/RuleHasOtherThanC_H_O_N_S2Test.java
@@ -27,7 +27,7 @@ public class RuleHasOtherThanC_H_O_N_S2Test extends AbstractRuleTest {
 	public void test() throws Exception {
 		Object[][] answer = {
 				{"O=C(C1=C(SN=N2)C2=CC=C1)SC",false},
-				{"O=C(O)N[C@H](C(N[C@@H](C1=NC2=CC=C(F)C=C2S1)C)=O)C(C)C",false},
+				{"O=C(O)N[C@H](C(N[C@@H](C1=NC2=CC=CC=C2S1)C)=O)C(C)C",false},
 				{"C1(C2=CSC=N2)=NC3=CC=CC=C3N1",false}
 		};
 		
@@ -38,22 +38,6 @@ public class RuleHasOtherThanC_H_O_N_S2Test extends AbstractRuleTest {
 				Boolean result = rule2test.verifyRule(a);
 				Object mf = a.getProperty(MolFlags.MOLFLAGS);
 				assertNotNull(mf);
-				/*
-				AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(a);
-				for (IAtom atom : a.atoms()) 
-					if ("S".equals(atom.getSymbol())) {
-						System.out.println(
-								String.format("getAtomTypeName %s getFormalNeighbourCount %d getBondOrderSum %d getFormalNeighbourCount %d getHybridization %s getMaxBondOrder %d Valency %d",
-										atom.getAtomTypeName(),
-										atom.getFormalNeighbourCount(),
-										atom.getBondOrderSum(),
-										atom.getFormalNeighbourCount(),
-										atom.getHybridization(),
-										atom.getMaxBondOrder(),
-										atom.getValency()
-										));
-					}
-				*/
 				assertEquals(((Boolean)answer[i][1]),result);
 		}
 		
