86664f7a5c9644a8c094965fe3c8814bea96d8df
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-11-21 19:11:17 +0000

    batch file to gather toxtree executable and plugins in dist folder

---
+++ toxtree/deploy.bat
@@ -0,0 +1,15 @@
+mkdir dist
+copy toxtree-app\target\toxtree-app-2.2.0-SNAPSHOT-jar-with-dependencies.jar dist
+
+copy toxtree-plugins\toxtree-cramer\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-cramer2\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-eye\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-kroes\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-mic\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-michaelacceptors\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-mutant\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-verhaar\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-sicret\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-smartcyp\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-skinsensitisation\target\*.jar dist\ext
+copy toxtree-plugins\toxtree-biodegradation\target\*.jar dist\ext
