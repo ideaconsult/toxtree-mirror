2096d4ec5bc365bfe3b1cb2da6d3dda1fdb1f2e0
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-08-03 14:40:12 +0000

    more cleanup, to ensure the product molecule is in good shape and no garbadge configs attached to its pieces

--- toxtree/toxtree-core/src/main/java/toxTree/query/metabolite/MetabolyteRecycler.java
+++ toxtree/toxtree-core/src/main/java/toxTree/query/metabolite/MetabolyteRecycler.java
@@ -6,9 +6,9 @@ import java.io.StringWriter;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.vecmath.Vector2d;
+
 import org.openscience.cdk.CDKConstants;
-import org.openscience.cdk.Strand;
-import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
 import org.openscience.cdk.graph.ConnectivityChecker;
 import org.openscience.cdk.inchi.InChIGenerator;
 import org.openscience.cdk.inchi.InChIGeneratorFactory;
@@ -16,9 +16,9 @@ import org.openscience.cdk.inchi.InChIToStructure;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtomContainerSet;
-import org.openscience.cdk.interfaces.IAtomType;
-import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.io.CMLWriter;
+import org.openscience.cdk.layout.StructureDiagramGenerator;
 import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesGenerator;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
@@ -26,7 +26,6 @@ import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import toxTree.query.remote.RemoteCompoundLookup;
 import ambit2.base.config.Preferences;
 import ambit2.base.data.Property;
-import ambit2.base.exceptions.AmbitException;
 import ambit2.core.config.AmbitCONSTANTS;
 import ambit2.core.data.MoleculeTools;
 import ambit2.core.io.MDLWriter;
@@ -34,7 +33,7 @@ import ambit2.core.processors.structure.AtomConfigurator;
 import ambit2.core.processors.structure.InchiProcessor;
 
 public class   MetabolyteRecycler implements PropertyChangeListener {
-	
+	protected StructureDiagramGenerator sdg = new StructureDiagramGenerator();
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
   		if (evt.getNewValue()==null) return;
@@ -65,7 +64,23 @@ public class   MetabolyteRecycler implements PropertyChangeListener {
 		AtomConfigurator cfg = new AtomConfigurator();        		
 		IAtomContainerSet set  = ConnectivityChecker.partitionIntoMolecules(products);
 		for (IAtomContainer result : set.atomContainers()) {
+			
+			//result = AtomContainerManipulator.removeHydrogensPreserveMultiplyBonded(result);
+			
+			try {
+				//cfg.process(result);
+				AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(result);
+			//	CDKHueckelAromaticityDetector.detectAromaticity(result);
+			} catch (Exception x) {}
+			
+			/*
+			for (IAtom atom : result.atoms()) {
+				System.out.println(String.format("%s %s %s",atom.getSymbol(),atom.getFormalCharge(),result.getBondOrderSum(atom)));
+			}
+			*/
+			
 			//quick hack for 5-valent carbon bug
+
 			for (IAtom atom : result.atoms()) {
 				if ("C".equals(atom.getSymbol()) && result.getBondOrderSum(atom)>=5){
 					//System.out.println(result.getBondOrderSum(atom));
@@ -74,87 +89,87 @@ public class   MetabolyteRecycler implements PropertyChangeListener {
 						if ("H".equals(neighbor.getSymbol())) { 
 
 							result.removeAtomAndConnectedElectronContainers(neighbor);
-							
-						      atom.setAtomTypeName((String) CDKConstants.UNSET);
-					            atom.setMaxBondOrder((IBond.Order) CDKConstants.UNSET);
-					            atom.setBondOrderSum((Double) CDKConstants.UNSET);
-					            atom.setCovalentRadius((Double) CDKConstants.UNSET);
-					            atom.setValency((Integer) CDKConstants.UNSET);
-					           // atom.setFormalCharge((Integer) CDKConstants.UNSET);
-					            atom.setHybridization((IAtomType.Hybridization) CDKConstants.UNSET);
-					            atom.setFormalNeighbourCount((Integer) CDKConstants.UNSET);
-					            atom.setFlag(CDKConstants.IS_HYDROGENBOND_ACCEPTOR, false);
-					            atom.setFlag(CDKConstants.IS_HYDROGENBOND_DONOR, false);
-					            atom.setProperty(CDKConstants.CHEMICAL_GROUP_CONSTANT, CDKConstants.UNSET);
-					            atom.setFlag(CDKConstants.ISAROMATIC, false);
-					            atom.setProperty("org.openscience.cdk.renderer.color", CDKConstants.UNSET);
-					            atom.setAtomicNumber((Integer) CDKConstants.UNSET);
-					            atom.setExactMass((Double) CDKConstants.UNSET);		
-
+							   atom.setFormalNeighbourCount(null);
+							   atom.setAtomTypeName(null);
+							   atom.setHybridization(null);
+							   atom.setBondOrderSum(null);
+							   atom.setValency(null);
+							   
+							   atom.setFlag(CDKConstants.ISAROMATIC, false);	
+								try {
+									//cfg.process(result);
+									AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(result);
+								} catch (Exception x) {}
+								//dobreak = true;
 							break;
 						}
 					}
 				}
+				
 			}
-			result = AtomContainerManipulator.removeHydrogensPreserveMultiplyBonded(result);
-			
-			try {
-				//cfg.process(result);
-				AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(result);
-				CDKHueckelAromaticityDetector.detectAromaticity(result);
-			} catch (Exception x) {}
-			
-			  
-			result = quick_and_dirty_viamol(result);
-			
+
 			String inchi = null;
 			String auxinfo = null;
 			try {
 				if (inchip!=null) {
 					InChIGenerator gen = inchip.process(result);
 					inchi = gen.getInchi();
-
+					auxinfo = gen.getAuxInfo();
 		       		
-		       		IAtomContainer tmp = quick_and_dirty_viainchi(inchi);
-		       		if (tmp != null) {
-		       			result = tmp;
-
-			       		result.setProperty(Property.opentox_InChI_std,inchi);
-			       		result.setProperty(Property.opentox_InChIAuxInfo_std,gen.getAuxInfo());
-		       		}
 				}
-			} catch (Throwable x) {}
+			} catch (Throwable x) {x.printStackTrace();}
+			
+			if (inchi==null)  {
+				result = quick_and_dirty_viainchi(inchi);
+			} else 
+			result = quick_and_dirty_viamol(result);
+	       		
+	       		
+	       	if (inchi!=null)  	   {    		
+	       		result.setProperty(Property.opentox_InChI_std,inchi);
+	       		result.setProperty(Property.opentox_InChIAuxInfo_std,auxinfo);	       		
+			}
 			
 			if ((inchi!=null) && Preferences.getProperty(Preferences.REMOTELOOKUP).equals("true")) try {
 				RemoteCompoundLookup lookup = new RemoteCompoundLookup();
 				IAtomContainer a = lookup.lookup(inchi);
-				Iterator keys = a.getProperties().keySet().iterator();
-				while (keys.hasNext()) {
-					Object key = keys.next();
-					if (Property.opentox_SMILES.equals(key)) continue; //we have that
-					Object value = result.getProperty(key);
-					if (value == null) {
-						value = a.getProperty(key);
-						String[] values = value.toString().split("\n");
-						if (values.length==1)
-							result.setProperty(key,value);
-						else
-							for (int i=0; i < values.length;i++)
-								result.setProperty(i==0?key:String.format("%s_%d",key,i),values[i]);
+				if (a !=null) {
+					Iterator keys = a.getProperties().keySet().iterator();
+					while (keys.hasNext()) {
+						Object key = keys.next();
+						if (Property.opentox_SMILES.equals(key)) continue; //we have that
+						Object value = result.getProperty(key);
+						if (value == null) {
+							value = a.getProperty(key);
+							String[] values = value.toString().split("\n");
+							if (values.length==1)
+								result.setProperty(key,value);
+							else
+								for (int i=0; i < values.length;i++)
+									result.setProperty(i==0?key:String.format("%s_%d",key,i),values[i]);
+						}
 					}
 				}
 			} catch (Exception x) {//do nothing 
-				
+				x.printStackTrace();
 			}
 			
 			try {
 				if(smigen !=null) {
+					smigen.setUseAromaticityFlag(true);
 		       		result.setProperty(AmbitCONSTANTS.SMILES,smigen.createSMILES(result));
 				}
 			} catch (Exception x) {
 				x.printStackTrace();
 			}				
-			result.setProperty("Created by SMARTCyp metabolite prediction",products.getID());			
+			result.setProperty("Created by SMARTCyp metabolite prediction",products.getID());	
+			
+        	if (sdg == null) sdg = new StructureDiagramGenerator();
+               sdg.setMolecule((IMolecule)result);
+               try {
+                sdg.generateCoordinates(new Vector2d(0,1));
+                result = sdg.getMolecule();
+               } catch (Exception x) {}
 			handleproduct(result);
 		}
 	}
