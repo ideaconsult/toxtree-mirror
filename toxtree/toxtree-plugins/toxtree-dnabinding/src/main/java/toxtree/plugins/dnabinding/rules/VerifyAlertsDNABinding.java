7a76bc759e055178d3698c1be48c9241e95148e6
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-11-09 07:29:28 +0000

    lower/upper case fixed

--- toxtree/toxtree-plugins/toxtree-dnabinding/src/main/java/toxtree/plugins/dnabinding/rules/VerifyAlertsDNABinding.java
+++ toxtree/toxtree-plugins/toxtree-dnabinding/src/main/java/toxtree/plugins/dnabinding/rules/VerifyAlertsDNABinding.java
@@ -6,13 +6,13 @@ import org.openscience.cdk.renderer.selection.IChemObjectSelection;
 import toxTree.tree.rules.RuleVerifyAlertsCounter;
 import ambit2.base.interfaces.IProcessor;
 
-public class VerifyAlertsDNAbinding extends RuleVerifyAlertsCounter {
+public class VerifyAlertsDNABinding extends RuleVerifyAlertsCounter {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6848759397201273916L;
-	public VerifyAlertsDNAbinding() {
+	public VerifyAlertsDNABinding() {
 		super();
 		setID("6");
 		setTitle("At least one alert for DNA binding?");
