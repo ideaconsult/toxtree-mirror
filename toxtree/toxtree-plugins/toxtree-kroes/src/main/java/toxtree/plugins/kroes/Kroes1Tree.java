dd93e10de509c15cb0863fece759815076278452
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-12-08 09:39:04 +0000

    Updated to use the new BB rules for genotoxic carcinogenicity

--- toxtree/toxtree-plugins/toxtree-kroes/src/main/java/toxtree/plugins/kroes/Kroes1Tree.java
+++ toxtree/toxtree-plugins/toxtree-kroes/src/main/java/toxtree/plugins/kroes/Kroes1Tree.java
@@ -27,53 +27,59 @@ public class Kroes1Tree extends UserDefinedTree
     
     private static final long serialVersionUID = 0x4b97b63a593def3bL;
     public static final transient String c_rules[] = {
-        "toxtree.plugins.kroes.rules.RuleKroesFig1Q1",
-        "mutant.rules.RuleAlertsForGenotoxicCarcinogenicity",
-        "toxtree.plugins.kroes.rules.KroesRule3",
-        "toxtree.plugins.kroes.rules.KroesRule4",
-        "toxtree.plugins.kroes.rules.KroesRule5",
-        "toxtree.plugins.kroes.rules.KroesRule6",
-        "toxtree.plugins.kroes.rules.KroesRule7",
-        "toxtree.plugins.kroes.rules.KroesRule8",
-        "toxtree.plugins.kroes.rules.KroesRule9",
-        "toxtree.plugins.kroes.rules.KroesRule10",
-        "toxtree.plugins.kroes.rules.KroesRule11",
-        "toxtree.plugins.kroes.rules.KroesRule12",
+    	toxtree.plugins.kroes.rules.RuleKroesFig1Q1.class.getName(),   //1
+    	mutant.rules.RuleAlertsForGenotoxicCarcinogenicity.class.getName(), //2
+    	
+    	toxtree.plugins.kroes.rules.KroesRule3.class.getName(), //3
+        toxtree.plugins.kroes.rules.KroesRule4.class.getName(), //4
+        toxtree.plugins.kroes.rules.KroesRule5.class.getName(),  //5
+        toxtree.plugins.kroes.rules.KroesRule6.class.getName(),  //6
+        toxtree.plugins.kroes.rules.KroesRule7.class.getName(),  //7
+        toxtree.plugins.kroes.rules.KroesRule8.class.getName(),  //8
+        toxtree.plugins.kroes.rules.KroesRule9.class.getName(),  //9
+        toxtree.plugins.kroes.rules.KroesRule10.class.getName(), //10
+        toxtree.plugins.kroes.rules.KroesRule11.class.getName(), //11
+        toxtree.plugins.kroes.rules.KroesRule12.class.getName(), //12
         
-        "mutant.rules.SA1", //13
-        "mutant.rules.SA2", //14
-        "mutant.rules.SA3", //15
-        "mutant.rules.SA4", //16
-        "mutant.rules.SA5", //17
-        "mutant.rules.SA6", //18
-        "mutant.rules.SA7", //19
-        "mutant.rules.SA8", //20
-        "mutant.rules.SA9", //21
-        "mutant.rules.SA11", //22
-        "mutant.rules.SA12", //23
-        "mutant.rules.SA13", //24
-        "mutant.rules.SA14", //25
-        "mutant.rules.SA15", //26
-        "mutant.rules.SA16", //27
-        "mutant.rules.SA18", //28
-        "mutant.rules.SA19", //29 
-        "mutant.rules.SA21", //30
-        "mutant.rules.SA22", //31
-        "mutant.rules.SA23", //32
-        "mutant.rules.SA24", //33
-        "mutant.rules.SA25", //34
-        "mutant.rules.SA26", //35
-        "mutant.rules.SA27", //36
-        "mutant.rules.SA28", //37
-        "mutant.rules.SA28bis", //38
-        "mutant.rules.SA28ter", //39   
-        "mutant.rules.SA29",    //40
-        "mutant.rules.SA30", //41
-        "toxtree.plugins.kroes.rules.KroesRule2" //42   
+        mutant.rules.SA1_gen.class.getName(), //13
+        mutant.rules.SA2_gen.class.getName(), //14
+        mutant.rules.SA3_gen.class.getName(), //15
+        mutant.rules.SA4_gen.class.getName(), //16
+        mutant.rules.SA5_gen.class.getName(), //17
+        mutant.rules.SA6_gen.class.getName(), //18
+        mutant.rules.SA7_gen.class.getName(), //19
+        mutant.rules.SA8_gen.class.getName(), //20
+        mutant.rules.SA9_gen.class.getName(), //21
+        mutant.rules.SA10_gen.class.getName(), //22
+        mutant.rules.SA11_gen.class.getName(), //23
+        mutant.rules.SA12_gen.class.getName(), //24
+        mutant.rules.SA13_gen.class.getName(), //25
+        mutant.rules.SA14_gen.class.getName(), //26
+        mutant.rules.SA15_gen.class.getName(), //27
+        mutant.rules.SA16_gen.class.getName(), //28
+        mutant.rules.SA18_gen.class.getName(), //29
+        mutant.rules.SA19_gen.class.getName(),  //30
+        mutant.rules.SA21_gen.class.getName(), //31
+        mutant.rules.SA22_gen.class.getName(), //32
+        mutant.rules.SA23_gen.class.getName(), //33
+        mutant.rules.SA24_gen.class.getName(), //34
+        mutant.rules.SA25_gen.class.getName(), //35
+        mutant.rules.SA26_gen.class.getName(), //36
+        mutant.rules.SA27_gen.class.getName(), //37
+        mutant.rules.SA28_gen.class.getName(), //38
+        mutant.rules.SA28bis_gen.class.getName(), //39
+        mutant.rules.SA28ter_gen.class.getName(), //40
+        mutant.rules.SA29_gen.class.getName(), //41
+        mutant.rules.SA30_gen.class.getName(), //42
+        mutant.rules.SA37_gen.class.getName(), //43
+        mutant.rules.SA38_gen.class.getName(), //44
+        mutant.rules.SA39_gen_and_nogen.class.getName(), //45
+        toxtree.plugins.kroes.rules.KroesRule2.class.getName() //46
+  
     };
     private static final transient int c_transitions[][] = {
         {2, 0, 0, 3}, //q1
-      //  {5, 3, 0, 0}, //q2
+      
         {13, 13, 0, 0}, //q2
         {4, 0, 0, 3}, //q3
         {0, 0, 2, 3}, //q4
@@ -118,7 +124,13 @@ public class Kroes1Tree extends UserDefinedTree
         
         {41,41,0,0}, //q40
         {42,42,0,0}, //q41
-        {5,3,0,0}, //q42
+        
+        {43,43,0,0}, //q42
+        {44,44,0,0}, //q43
+        {45,45,0,0}, //q44
+        {46,46,0,0}, //q45
+        
+        {5,3,0,0}, //q46
         
     };
     private static final transient String c_categories[] = {
