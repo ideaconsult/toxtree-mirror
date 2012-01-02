d75a5f98dcacfdedd72281c4dcf609c8191cb7fb
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-01-02 19:11:37 +0000

    moved qsar tests to a separate folder

--- toxtree/toxtree-plugins/toxtree-mutant/src/test/java/mutant/test/rules/qsar/RuleAromaticDiazoTest.java
+++ toxtree/toxtree-plugins/toxtree-mutant/src/test/java/mutant/test/rules/qsar/RuleAromaticDiazoTest.java
@@ -22,7 +22,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
 */
 
-package mutant.test.rules;
+package mutant.test.rules.qsar;
 
 import java.util.ArrayList;
 import java.util.List;
