d77d18de83cc6b59c164569be527dd50b5c673fb
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-02-25 11:59:30 +0000

    test updated with proper preprocessing and yet another SMARTS

--- toxtree-plugins/toxtree-cramer2/src/test/java/toxTree/cramer2/RuleBenzeneAnaloguesTest.java
+++ toxtree-plugins/toxtree-cramer2/src/test/java/toxTree/cramer2/RuleBenzeneAnaloguesTest.java
@@ -5,9 +5,9 @@ import junit.framework.Assert;
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
-import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import toxTree.core.IDecisionRule;
+import toxTree.query.MolAnalyser;
 import ambit2.base.config.Preferences;
 import ambit2.smarts.query.SmartsPatternAmbit;
 import cramer2.rules.RuleBenzeneAnalogues;
@@ -24,23 +24,32 @@ public class RuleBenzeneAnaloguesTest  extends AbstractRuleTest {
 	 */
 	@Override
 	public void test() throws Exception  {
-		Preferences.setProperty(Preferences.SMILESPARSER,"true");
+		//Preferences.setProperty(Preferences.SMILESPARSER,"false");
 	    Object[][] answer = {
-	            {"Oc1ccc(O)cc1",new Boolean(true)}, //20N
-	            {"Cc1cccc(C)c1O",new Boolean(true)}, //20N
-	            {"O1C(C)=CC(=O)C(C(=O)C)C(=O)1",new Boolean(true)}, //20N
+	            {"Oc1ccc(O)cc1",new Boolean(true)},
+	            {"Cc1cccc(C)c1O",new Boolean(true)},
+	            {"O1C(C)=CC(=O)C(C(=O)C)C(=O)1",new Boolean(true)}, 
 	            };
         
+	   //String smarts = "[$([aH1;r6]),$([aX2;r6]),$([a;r6]~A)!$(a~A~*)]1~[$([aH1]),$([aX2]),$(a~A)!$(a~A~*)]~[$([aH1]),$([aX2]),$(a~A)!$(a~A~*)]~[$([aH1]),$([aX2]),$(a~A)!$(a~A~*)]~[$([aH1]),$([aX2]),$(a~A)!$(a~A~*)]~[$([aH1]),$([aX2]),$(a~A)!$(a~A~*)]1";
+	    String smarts = "[$([aH1;r6]),$([aX2;r6]),$([a;r6]~[A;D1])]1~[$([aH1]),$([aX2]),$(a~[A;D1])]~[$([aH1]),$([aX2]),$(a~[A;D1])]~[$([aH1]),$([aX2]),$(a~[A;D1])]~[$([aH1]),$([aX2]),$(a~[A;D1])]~[$([aH1]),$([aX2]),$(a~[A;D1])]1";
+	   // String smarts = "a";
 	   
-	   SmartsPatternAmbit p = new SmartsPatternAmbit(RuleBenzeneAnalogues.smarts);
+	   SmartsPatternAmbit p = new SmartsPatternAmbit(smarts);
 	   SmilesParser smi = new SmilesParser(SilentChemObjectBuilder.getInstance());
+	   
 	   for (Object[] row : answer) {
 		   IMolecule testMol = smi.parseSmiles(row[0].toString());
 		   Assert.assertTrue(p.match(testMol)>0);
-		   AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(testMol);
-		   Assert.assertTrue(p.match(testMol)>0);
+		   MolAnalyser.analyse(testMol);
+		   //AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(testMol);
+		   //CDKHueckelAromaticityDetector.detectAromaticity(testMol);
+		   int r = p.match(testMol);
+		   Assert.assertTrue(r>0);
+		
 	   }
-	  // ruleTest(answer); this is the real rule application, which fails , probably for the reason the matching fails after atom perception
+	  // this is the real rule application, which fails , probably for the reason the matching fails after atom perception
+	  //ruleTest(answer); 
 	    
 	}
 }
