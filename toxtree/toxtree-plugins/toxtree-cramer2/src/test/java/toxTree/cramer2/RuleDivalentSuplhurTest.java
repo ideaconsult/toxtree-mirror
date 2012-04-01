429a325a474f94d6100dac8fca92275214cd1e3e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-04-01 17:46:48 +0000

    Added tests for cramer2 specific rules

---
+++ toxtree/toxtree-plugins/toxtree-cramer2/src/test/java/toxTree/cramer2/RuleDivalentSuplhurTest.java
@@ -0,0 +1,19 @@
+package toxTree.cramer2;
+
+import cramer2.rules.RuleDivalentSulphur;
+import toxTree.core.IDecisionRule;
+import toxTree.cramer.AbstractRuleTest;
+
+public class RuleDivalentSuplhurTest extends AbstractRuleTest {
+
+	@Override
+	protected IDecisionRule createRule() {
+		return new RuleDivalentSulphur();
+	}
+
+	@Override
+	public void test() throws Exception {
+	
+	}
+
+}
