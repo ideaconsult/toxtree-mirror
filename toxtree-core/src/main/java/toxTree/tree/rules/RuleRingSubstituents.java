cad2ef280c7bd04fa7c25b05aa4e46120098a543
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-03-06 11:19:15 +0000

    Rewriting to use the CDK 1.5 flags-as-masks approach

--- toxtree-core/src/main/java/toxTree/tree/rules/RuleRingSubstituents.java
+++ toxtree-core/src/main/java/toxTree/tree/rules/RuleRingSubstituents.java
@@ -21,12 +21,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-*/
+ */
 package toxTree.tree.rules;
 
 import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.graph.ConnectivityChecker;
-import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtomContainerSet;
 import org.openscience.cdk.interfaces.IRing;
@@ -38,119 +37,142 @@ import toxTree.query.MolAnalyser;
 
 /**
  * A rule to analyze ring substituents
- * @author Nina Jeliazkova
- * <b>Modified</b> 2005-8-17
+ * 
+ * @author Nina Jeliazkova <b>Modified</b> 2005-8-17
  */
 public abstract class RuleRingSubstituents extends RuleRings {
 	public static String ERR_PRECONDITION_FAILED = "Forbidden ring substituent found (precondition failed)";
 	public static String CONDITION_FAILED = "Forbidden substructure found in a ring substituent";
 	public static String CYCLIC_SUBSTITUENT = "Ring substituent bearing another ring!";
 	public static String LONGCHAIN_SUBSTITUENT = "Ring substituent with > 5 carbon atoms!\t - ";
-	
 
-	protected boolean[] flags = null;
+	// protected boolean[] flags = null;
 	protected boolean analyzeOnlyRingsWithFlagSet = false;
 	/**
 	 * Comment for <code>serialVersionUID</code>
 	 */
 	private static final long serialVersionUID = 8095271692686086087L;
-
+	private short flagValue = 0;
+	private boolean flag_heterocyclyc = false;
 	/**
 	 * 
 	 */
 	public RuleRingSubstituents() {
 		super();
+		clearFlags();
+	}
 
-	    flags = new boolean[CDKConstants.MAX_FLAG_INDEX+1];
-	    clearFlags();
+	public void setFlag(int mask, boolean value) {
+		// set/unset a bit in the flags value
+		if (value)
+			flagValue |= mask;
+		else
+			flagValue &= ~(mask);
+	}
+
+	public boolean getFlag(int mask) {
+		return (flagValue & mask) != 0;
 	}
 
 	protected boolean analyze(IRing r) {
-		
+
 		if (analyzeOnlyRingsWithFlagSet) {
-			if (flags[flags.length-1]) {
+			if (flag_heterocyclyc) {
 				Object o = r.getProperty(MolAnalyser.HETEROCYCLIC);
-				if (o==null) return false;
-				if (((Boolean) o).booleanValue()) return true;
+				if (o == null)
+					return false;
+				if (((Boolean) o).booleanValue())
+					return true;
 			}
-			boolean[] ringFlags = r.getFlags();
-			for (int i=0; i<(flags.length-1); i++)
-				if (i>=ringFlags.length) break;
-				else if (ringFlags[i] && flags[i]) return true;
-							
+			for (int i = 0; i < CDKConstants.FLAG_MASKS.length; i++) 
+				if (r.getFlag(i) && getFlag(i))
+					return true;
+
 			return false;
 
-		} else return true;
-	}	
+		} else
+			return true;
+	}
 
 	/**
-	 *returns true if the substituent is allowed and false if it is forbidden 
-	 *to be implemented by the inheriting class
+	 * returns true if the substituent is allowed and false if it is forbidden
+	 * to be implemented by the inheriting class
 	 */
-	public abstract boolean substituentIsAllowed(IAtomContainer  a, int[] place) throws DecisionMethodException;
-	
+	public abstract boolean substituentIsAllowed(IAtomContainer a, int[] place) throws DecisionMethodException;
+
 	public void clearFlags() {
-		for (int i=0; i<flags.length; i++) flags[i] = false;
+		flagValue = 0;
+		flag_heterocyclyc = false;
 	}
+
 	public void setAnalyzeHeterocyclic(boolean flag) {
-		flags[CDKConstants.MAX_FLAG_INDEX] = flag;
+		flag_heterocyclyc = flag;
 	}
+
 	public void setAnalyzeAromatic(boolean flag) {
-		flags[CDKConstants.ISAROMATIC] = flag;
-	}		
+		setFlag(CDKConstants.ISAROMATIC,flag);
+	}
+
 	/**
 	 * @return Returns the analyzeOnlyRingsWithFlagSet.
 	 */
 	public boolean analyzeOnlyRingsWithFlagSet() {
 		return analyzeOnlyRingsWithFlagSet;
 	}
+
 	/**
-	 * @param analyzeOnlyRingsWithFlagSet The analyzeOnlyRingsWithFlagSet to set.
+	 * @param analyzeOnlyRingsWithFlagSet
+	 *            The analyzeOnlyRingsWithFlagSet to set.
 	 */
-	public void setAnalyzeOnlyRingsWithFlagSet(
-			boolean analyzeOnlyRingsWithFlagSet) {
+	public void setAnalyzeOnlyRingsWithFlagSet(boolean analyzeOnlyRingsWithFlagSet) {
 		this.analyzeOnlyRingsWithFlagSet = analyzeOnlyRingsWithFlagSet;
 	}
+
 	public String allowedSubstituents() {
 		return "Allowed substituents:\t" + ids.toString();
 	}
+
 	/**
 	 * {@link toxTree.core.IDecisionRule#verifyRule(IAtomContainer)}
 	 */
 	public boolean verifyRule(IAtomContainer mol) throws DecisionMethodException {
-		return verifyRule(mol,null);
+		return verifyRule(mol, null);
 	};
-	public boolean verifyRule(IAtomContainer  mol, IAtomContainer selected) throws DecisionMethodException {
+
+	public boolean verifyRule(IAtomContainer mol, IAtomContainer selected) throws DecisionMethodException {
 		logger.fine(toString());
 		IRingSet rs = hasRingsToProcess(mol);
-		if (rs == null) return false;
-		
-	    IRing r =null;
-	    for (int i=0; i < rs.getAtomContainerCount(); i++) {
-	        r = (IRing) rs.getAtomContainer(i);
-	        if (!analyze(r)) continue;
-	        logger.finer("Ring\t"+(i+1));
-	        
-	        //new atomcontainer with ring atoms/bonds deleted
-	        IAtomContainer mc = FunctionalGroups.cloneDiscardRingAtomAndBonds(mol,r);	        
-			logger.finer("\tmol atoms\t"+mc.getAtomCount());
-		    
-			IAtomContainerSet  s = ConnectivityChecker.partitionIntoMolecules(mc);
-			//logger.debug("partitions\t",s.getMoleculeCount());
+		if (rs == null)
+			return false;
+
+		IRing r = null;
+		for (int i = 0; i < rs.getAtomContainerCount(); i++) {
+			r = (IRing) rs.getAtomContainer(i);
+			if (!analyze(r))
+				continue;
+			logger.finer("Ring\t" + (i + 1));
+
+			// new atomcontainer with ring atoms/bonds deleted
+			IAtomContainer mc = FunctionalGroups.cloneDiscardRingAtomAndBonds(mol, r);
+			logger.finer("\tmol atoms\t" + mc.getAtomCount());
+
+			IAtomContainerSet s = ConnectivityChecker.partitionIntoMolecules(mc);
+			// logger.debug("partitions\t",s.getMoleculeCount());
 			for (int k = 0; k < s.getAtomContainerCount(); k++) {
 				IAtomContainer m = s.getAtomContainer(k);
-			    if (m!=null) {
-				    if ((m.getAtomCount() == 1) && (m.getAtom(0).getSymbol().equals("H"))) continue;
-				    logger.finer("Ring substituent\t"+(k+1));
-				    if (!substituentIsAllowed(m,null)) {
-				    	logger.finer(ERR_PRECONDITION_FAILED);
-				    	return false;
-				    }
-			    }
-			}	        
-	    }
-	    return true;
-		
+				if (m != null) {
+					if ((m.getAtomCount() == 1) && (m.getAtom(0).getSymbol().equals("H")))
+						continue;
+					logger.finer("Ring substituent\t" + (k + 1));
+					if (!substituentIsAllowed(m, null)) {
+						logger.finer(ERR_PRECONDITION_FAILED);
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+
 	}
-	
+
 }
