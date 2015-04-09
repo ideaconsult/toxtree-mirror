f81f2a908bf189050c47a784327cf9a3c3a99f12
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-04-09 07:03:46 +0000

    dependencies cleanup swing dependencies moved from toxtree-core to toxtree-swing unused classes removed

--- toxtree/toxtree-plugins/toxtree-mutant/src/test/java/mutant/test/JoelibSmartsTest.java
+++ toxtree/toxtree-plugins/toxtree-mutant/src/test/java/mutant/test/JoelibSmartsTest.java
@@ -22,7 +22,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
 */
 
-package toxTree.test.smarts;
+package mutant.test;
 
 import javax.vecmath.Point3d;
 
